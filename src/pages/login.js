b1ad878d9a4af66092cbbe70f86bd9a6c7617bd9
Ender <nallahabi2003@gmail.com>
2025-02-27 17:54:18 -0500

    Figured out how to handle multiple pages

---
+++ src/pages/login.js
@@ -0,0 +1,18 @@
+import '../App.css';
+import { Link } from 'react-router-dom'; // needed to interact with the React App BrowserRouter
+
+export function Login() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </nav>
+        <p>This is the Login Page</p>
+      </header>
+    </div>
+  );
+}
