41e82147be836eb1bc13ab9d70d2f3cea43fdb23
Ender <nallahabi2003@gmail.com>
2025-09-10 12:49:19 -0400

    Hard-coded the event.origin to avoid using react env

--- src/pages/login.js
+++ src/pages/login.js
@@ -19,8 +19,8 @@ export function Login() {
         );
 
         const handleMessage = (event) => {
-            if (event.origin !== `${process.env.REACT_APP_HOMEPAGE_URL}`) {
-                console.log(`event.origin is different: ${event.origin} != ${process.env.REACT_APP_HOMEPAGE_URL}`);
+            if (event.origin !== "https://ctf.hacksu.com") {
+                console.log(`event.origin is different: ${event.origin}`);
                 return; // check backend origin
             }
 
