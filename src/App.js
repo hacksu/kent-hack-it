0c8977a9bb85df24679885c111bdb7b62eb071c6
Ender <nallahabi2003@gmail.com>
2025-09-10 12:39:59 -0400

    Fixed redirections for external sites

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Navigate, BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // When the user moves around different pages the browser URL updates correctly
 import { Login } from './pages/login.js' // custom js that returns HTML
 import { Home } from './pages/home.js'
@@ -7,6 +8,15 @@ import { AdminPanel } from './pages/admin_panel.js'
 import { Leaderboard } from './pages/leaderboard.js'
 import { RatingPage } from './pages/rate_challenge.js'
 import { ChallengeDetail } from './pages/challenge_template.js'
+
+// helper function for redirections
+function ExternalRedirect({ to }) {
+    useEffect(() => {
+        window.location.href = to;
+    }, [to]);
+
+    return null;
+}
 
 // index.html handles the html format of the page layout to a certain point
 function App() {
@@ -24,8 +34,8 @@ function App() {
                 <Route path="/rate-challenge" element={<RatingPage />} />
                 {/* Redirection route */}
                 <Route path="/admin" element={<Navigate to="/admin/panel" replace />} />
-                <Route path='/discord' element={() => {window.location.href = 'https://discord.gg/rJDdvnt'; return null; }} />
-                <Route path='/tools' element={() => {window.location.href = 'https://dcode.fr'; return null; }} />
+                <Route path="/discord" element={<ExternalRedirect to="https://discord.gg/rJDdvnt" />} />
+                <Route path="/tools" element={<ExternalRedirect to="https://dcode.fr" />} />
             </Routes>
         </Router>
     );
