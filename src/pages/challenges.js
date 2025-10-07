379eed05b6c5ecb93c84c24a79c36cfb08648899
Ender <nallahabi2003@gmail.com>
2025-10-07 13:06:52 -0400

    Styling Link tag

--- src/pages/challenges.js
+++ src/pages/challenges.js
@@ -221,20 +221,20 @@ export function Challenges() {
                         <Navbar />
                         <div className="container-fluid mt-4">
 
-                            <div className="row align-items-center mb-4">
-                                <div className="col-12 position-relative text-center">
-                                    {/* Centered Title */}
-                                    <h2 className="mb-0">Challenges</h2>
+                        <div className="row align-items-center mb-4">
+                            <div className="col-12 position-relative text-center">
+                                {/* Centered Title */}
+                                <h2 className="mb-0">Challenges</h2>
 
-                                    {/* Left-Aligned Link with Padding */}
-                                    <Link
-                                    className="nav-link fw-semibold text-primary position-absolute top-50 start-0 translate-middle-y ps-3"
-                                    to="/challenge_help"
-                                    >
-                                    Challenge Help
-                                    </Link>
-                                </div>
+                                {/* Left-Aligned Link */}
+                                <Link
+                                className="challenge-link position-absolute top-50 start-0 translate-middle-y ps-4"
+                                to="/challenge_help"
+                                >
+                                Challenge Help
+                                </Link>
                             </div>
+                        </div>
 
                             <div className="row">
                                 {/* Filter Sidebar */}
