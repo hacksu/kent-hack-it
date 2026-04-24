ad7af46bbf02d9a57c036c610a305b5ec790a414
Ender <nallahabi2003@gmail.com>
2026-04-23 23:56:53 -0400

    Feedback when creating challenges
    After submitting the view is scrolled to the feedback area

--- src/routes/admin/d.create.svelte
+++ src/routes/admin/d.create.svelte
@@ -1,11 +1,43 @@
 <script lang="ts">
     import ChallengeForm from '$lib/components/challenge.form.svelte';
+    
+    let result = $state(undefined);
+    function clearResult() {
+        result = undefined;
+    }
+
+    function scrollToFeedback() {
+        const elem = document.getElementById('feedback-display');
+        elem?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+    
     const { form } = $props();
 </script>
+
+<div id="feedback-display">
+    {#if result?.success}
+        <div class="view-feedback">
+            <div class="view-alert">{result.message}</div>
+        </div>
+    {:else if result?.error}
+        <div class="view-feedback">
+            <div class="view-alert view-err">{result.error}</div>
+        </div>
+    {/if}
+</div>
 
 <ChallengeForm
     title="Create a New Challenge"
     action_target="?/add_challenge"
     challenge={undefined}
+    onSubmit={(data: { success: boolean, message: string }|undefined) => {
+        if (data) {
+            result = data;
+        } else {
+            result = { error: 'An error occurred' };
+        }
+        scrollToFeedback();
+        setTimeout(clearResult, 5000);
+    }}
     result={form}
 />
