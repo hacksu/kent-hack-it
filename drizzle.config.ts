9378edd37434cd06577734314f92e702561e24a1
Monster0506 <tjraklovits@gmail.com>
2026-07-20 17:01:32 -0400

    Remove plaintext DB credential logging from drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from "drizzle-kit";
 
-console.log(process.env.PG_HOST!);
-console.log(process.env.PG_USER!);
-console.log(process.env.PG_PASSWORD!);
-console.log(process.env.PG_DATABASE!);
-
 export default defineConfig({
   schema: "./src/lib/database/auth-schema.ts",
   out: "./drizzle",
