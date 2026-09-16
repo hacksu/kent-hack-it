f08ae44173f1552c112d038d9422136a2fd48275
Monster0506 <tjraklovits@gmail.com>
2026-09-16 12:48:22 -0400

    testing hooks REVERTME

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,17 @@
 import { auth } from "$lib/server/auth";
+import type { Handle } from '@sveltejs/kit';
 import { svelteKitHandler } from "better-auth/svelte-kit";
 import { building } from '$app/environment'
 
-export async function handle({ event, resolve }) {
+export const handle: Handle = async ({ event, resolve }) => {
+    // prevent AI crawling into non-root areas of KHI
+    const response = await resolve(event);
+	if (event.url.pathname !== '/') {
+		response.headers.set(
+			'X-Robots-Tag',
+			'noai, noimageai, noindex'
+		);
+	}
+
     return svelteKitHandler({ event, resolve, auth, building });
 }
